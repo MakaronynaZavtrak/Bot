779bdd62cb78ff8d0f4773e269a9beb71d73bedb
Oleg Semenov <semyonov.owa@gmail.com>
2023-11-07 15:42:01 +0500

    Подправил main-метод для ConsoleBot

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -1,7 +1,13 @@
 package org.example;
 
+/**
+ * Класс, содержащий main-метод для запуска консрольного бота
+ */
 public class Bot
 {
+    /**
+     * метод для запуска консольного бота
+     */
     public static void main(String[] args)
     {
         ConsoleBot cBot = new ConsoleBot(true);
